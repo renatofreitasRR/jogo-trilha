6657770197a28d24a7fd5b4d80353159b30802c8
renatofreitasRR <natogfreitas@gmail.com>
2024-04-01 23:33:00 -0300

    change tag a to component Link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,16 @@
 import styles from './styles.module.css';
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <main className={styles.container}>
       <h1>Bem vindo</h1>
-      <a className={styles.link} href='board'>
+      <Link className={styles.link} href='board'>
         <strong>Multiplayer Local</strong>
-      </a>
-      <a className={`${styles.link} ${styles.link_disabled}`} title='Em desenvolvimento' href='#'>
+      </Link>
+      <Link className={`${styles.link} ${styles.link_disabled}`} title='Em desenvolvimento' href='#'>
         <strong>Multiplayer Online</strong>
-      </a>
+      </Link>
     </main>
   );
 }
