954fa93a939499675211b0a928d35ab4e9bead54
renatofreitasRR <natogfreitas@gmail.com>
2024-04-01 23:44:00 -0300

    fix link to boarrd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className={styles.container}>
       <h1>Bem vindo</h1>
-      <Link className={styles.link} href='board'>
+      <Link className={styles.link} href='pages/board'>
         <strong>Multiplayer Local</strong>
       </Link>
       <Link className={`${styles.link} ${styles.link_disabled}`} title='Em desenvolvimento' href='#'>
