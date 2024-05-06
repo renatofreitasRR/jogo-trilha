588116cb7060bfb36fe74c7034ad66e107fcfa46
renatofreitasRR <natogfreitas@gmail.com>
2024-05-06 00:16:55 -0300

    fix online player turn

--- src/app/pages/online/board/components/PlayerTurn/index.tsx
+++ src/app/pages/online/board/components/PlayerTurn/index.tsx
@@ -4,12 +4,13 @@ import styles from "./index.module.css";
 import Chip from "../Chip";
 import { getLocalItem } from "@/app/utils/localStorage";
 
+//to do
 export function PlayerTurn() {
 
     return (
         <div className={styles.player_turn}>
             <h2>Turno Jogador {getLocalItem("nome")}</h2>
-            <Chip />
+            <Chip playerColor={"red"} />
         </div>
     )
 }
