614a60d1dbe3dff0cc49934081fd82a021ebd481
Vinicius Calgaro <vinicalgaro@outlook.com>
2024-05-06 16:29:02 -0300

    Implementação do CRUD para Ícones

---
+++ src/app/pages/icones/editar/[id]/page.tsx
@@ -0,0 +1,119 @@
+"use client";
+import { useState, useEffect } from "react";
+import {
+  Box,
+  Image as Icone,
+  Input,
+  Button,
+  FormControl,
+  FormLabel,
+  useToast,
+} from "@chakra-ui/react";
+import { useParams, useRouter } from "next/navigation";
+import { api } from "@/app/services/api";
+
+interface Icone {
+  icnnome: string;
+  icnurl: string;
+  icnpreco: number;
+  tmacodigo: number;
+}
+
+const IconeEdit: React.FC = () => {
+  const toast = useToast();
+  const router = useRouter();
+  const params = useParams();
+  const { id } = params;
+
+  const [icone, setIcone] = useState<Icone | null>(null);
+  const [icnnome, setIcnnome] = useState("");
+  const [icnurl, setIcnUrl] = useState("");
+  const [icnpreco, setIcnpreco] = useState(0);
+
+  useEffect(() => {
+    const fetchIcone = async () => {
+      try {
+        const response = await api.get(`/icones?id=${id}`);
+
+        setIcone(response.data[0]);
+        setIcnnome(response.data[0].icnnome);
+        setIcnpreco(response.data[0].icnpreco);
+        setIcnUrl(response.data[0].icnurl);
+      } catch (error) {
+        console.error("Erro ao buscar ícone:", error);
+      }
+    };
+
+    if (id) {
+      fetchIcone();
+    }
+  }, [id]);
+
+  const handleSave = async () => {
+    try {
+      await api.patch(`/icones/${id}`, {
+        ...icone,
+        icnurl: icnurl,
+        icnnome: icnnome,
+        icnpreco: icnpreco,
+      });
+
+      toast({
+        title: "Atualização de Ícone",
+        description: "Dados atualizados com Sucesso",
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+      });
+
+      router.push("/pages/icones/lista");
+    } catch (error) {
+      console.error("Erro ao atualizar ícones:", error);
+    }
+  };
+
+  return (
+    <Box>
+      {!icone ? (
+        <div>Carregando...</div>
+      ) : (
+        <>
+          <Icone
+            src={icone.icnurl}
+            alt={icone.icnnome}
+            boxSize="200px"
+            objectFit="cover"
+          />
+          <FormControl id="icnnome" mt={4}>
+            <FormLabel>Nome do Ícone</FormLabel>
+            <Input
+              value={icnnome}
+              onChange={(e) => setIcnnome(e.target.value)}
+            />
+          </FormControl>
+          <FormControl id="icnpreco" mt={4}>
+            <FormLabel>Preço do Ícone</FormLabel>
+            <Input
+              type="number"
+              value={icnpreco}
+              onChange={(e) => setIcnpreco(Number(e.target.value))}
+            />
+          </FormControl>
+          <FormControl id="icnpreco" mt={4}>
+            <FormLabel>URL do Ícone</FormLabel>
+            <Input
+              type="text"
+              value={icnurl}
+              onChange={(e) => setIcnUrl(e.target.value)}
+            />
+          </FormControl>
+          <Button mt={4} colorScheme="teal" onClick={handleSave}>
+            Salvar
+          </Button>
+        </>
+      )}
+    </Box>
+  );
+};
+
+export default IconeEdit;
---
+++ src/app/pages/icones/lista/page.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import {
+  Box,
+  Image as Icone,
+  Text,
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  Heading,
+  useToast,
+  Button,
+} from "@chakra-ui/react";
+import Link from "next/link";
+import { api } from "@/app/services/api";
+import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
+
+interface Icone {
+  id?: number;
+  icnnome: string;
+  icnurl: string;
+  icnpreco: number;
+  tmacodigo: number;
+}
+
+const IconeList: React.FC = () => {
+  const [icones, setIcones] = useState<Icone[]>([]);
+  const toast = useToast();
+
+  useEffect(() => {
+    const fetchIcones = async () => {
+      try {
+        const response = await api.get("/icones");
+        setIcones(response.data);
+      } catch (error) {
+        console.error("Erro ao buscar ícones:", error);
+      }
+    };
+
+    fetchIcones();
+  }, []);
+
+  async function handleDelete(id: any) {
+    try {
+      await api.delete(`/icones/${id}`);
+
+      toast({
+        title: "Exclusão de Ícone",
+        description: "Ícone excluído com Sucesso",
+        status: "success",
+        duration: 9000,
+        isClosable: true,
+      });
+
+      const filter = icones.filter((x) => x.id != id);
+      setIcones(filter);
+    } catch (error) {
+      console.error("Erro ao atualizar ícone:", error);
+    }
+  }
+
+  return (
+    <Box>
+      <Heading mb={4}>Lista de Ícones</Heading>
+      <Button mt={4} colorScheme="teal">
+        <Link href={`/pages/icones/criar`}>Cadastrar</Link>
+      </Button>
+      <Table variant="simple">
+        <Thead>
+          <Tr>
+            <Th>Imagem</Th>
+            <Th>Nome</Th>
+            <Th>Preço</Th>
+            {/* <Th>Código do Tema</Th> */}
+            <Th>Ações</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {icones.map((icone, index) => (
+            <Tr key={index}>
+              <Td>
+                <Icone
+                  src={icone.icnurl}
+                  alt={icone.icnnome}
+                  boxSize="200px"
+                  objectFit="cover"
+                />
+              </Td>
+              <Td>{icone.icnnome}</Td>
+              <Td>R${icone.icnpreco.toFixed(2)}</Td>
+              {/* <Td>{image.tmacodigo}</Td> */}
+              <Td>
+                <Link href={`/pages/icones/editar/${icone.id}`}>
+                  <EditIcon />
+                </Link>
+                {"      "}|{"      "}
+                <DeleteIcon
+                  style={{
+                    cursor: "pointer",
+                  }}
+                  onClick={() => handleDelete(icone.id ?? 0)}
+                />
+              </Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </Box>
+  );
+};
+
+export default IconeList;
