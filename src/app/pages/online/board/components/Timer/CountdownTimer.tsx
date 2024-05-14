5153b5bca6293a4e8ffc40f68196b9ddf8d9c449
renatofreitasRR <natogfreitas@gmail.com>
2024-05-13 21:34:03 -0300

    comentando

--- src/app/pages/online/board/components/Timer/CountdownTimer.tsx
+++ src/app/pages/online/board/components/Timer/CountdownTimer.tsx
@@ -1,28 +1,28 @@
-import React, { useContext } from 'react';
-import { useCountdown } from '../../hooks/useCountdown';
-import DateTimeDisplay from './DateTimeDisplay';
-import styles from './index.module.css'
-import { BoardContext } from '../../contexts/boardContext';
+// import React, { useContext } from 'react';
+// import { useCountdown } from '../../hooks/useCountdown';
+// import DateTimeDisplay from './DateTimeDisplay';
+// import styles from './index.module.css'
+// import { BoardContext } from '../../contexts/boardContext';
 
-interface ShowCounterProps {
-    seconds: number;
-}
+// interface ShowCounterProps {
+//     seconds: number;
+// }
 
-const ShowCounter = ({ seconds }: ShowCounterProps) => {
-    return (
-        <div className={styles.show_counter}>
-            <DateTimeDisplay value={seconds} type={'s'} />
-        </div>
-    );
-};
+// const ShowCounter = ({ seconds }: ShowCounterProps) => {
+//     return (
+//         <div className={styles.show_counter}>
+//             <DateTimeDisplay value={seconds} type={'s'} />
+//         </div>
+//     );
+// };
 
-export const CountdownTimer = () => {
-    const { turnTime } = useContext(BoardContext);
+// export const CountdownTimer = () => {
+//     // const { turnTime } = useContext(BoardContext);
 
-    return (
-        <ShowCounter
-            seconds={turnTime}
-        />
-    );
-    // }
-};
+//     return (
+//         <ShowCounter
+//             seconds={turnTime}
+//         />
+//     );
+//     // }
+// };
