f1b0e07301c0613347db8c2cec3147c137ec1de2
filimape <filipe.cabral.lima@gmail.com>
2024-06-02 23:46:24 -0300

    telas login/cadastro

--- src/app/pages/login/page.tsx
+++ src/app/pages/login/page.tsx
@@ -4,6 +4,7 @@ import styles from './styles.module.css';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from "zod";
 import { api } from "@/app/services/api";
+import Link from 'next/link'
 
 interface LoginFormulario {
     email: string;
@@ -13,12 +14,12 @@ interface LoginFormulario {
 const LoginFormularioSchema = z.object({
     email: z
         .string()
-        .min(1, { message: "Campo email é obrigatório" })
-        .email("O Campo precisa estar em um formato de email válido"),
+        .min(1, { message: "O campo é obrigatório" })
+        .email("Digite um email válido"),
 
     senha: z
         .string()
-        .min(1, { message: "Campo senha é obrigatório" })
+        .min(1, { message: "O campo é obrigatório" })
 });
 
 const Login: React.FC = () => {
@@ -33,18 +34,38 @@ const Login: React.FC = () => {
         //envia pra api
     }
 
+    const FormInput = ({ name, register, errors, ...rest }: any) => (
+        <div className="form_control">
+            <input {...register(name)} {...rest} />
+            {errors[name] && <span className={styles.error_message}>{errors[name].message}</span>}
+        </div>
+    );
 
     return (
-        <form className={styles.form_login} onSubmit={handleSubmit(enviarFormulario)}>
+        <>
+        <main className={styles.container}>
+        <div className={styles.machine}>
+            <div className={styles.screen}>
+            <div className={styles.menu}>
+                <form className={styles.form_login} onSubmit={handleSubmit(enviarFormulario)}>
+                <label className={styles.label_login}>LOGIN</label>
 
-            <input {...register('email')} placeholder="1" className={styles.input_login} />
-            {errors.email?.message}
+                <input {...register('email')} placeholder="Digite seu usuário" className={styles.input_login} />
+                {errors?.email?.message && <span className={styles.error_message}>{errors?.email?.message}</span>}
 
-            <input {...register('senha')} placeholder="2" className={styles.input_login} />
-            {errors.senha?.message}
-
-            <button className={styles.button_login}>Salvar</button>
-        </form>
+                <input {...register('senha')} placeholder="Digite sua senha" className={styles.input_login} type="password"/>
+                {errors?.senha?.message && <span className={styles.error_message}>{errors?.senha?.message}</span>}
+                
+                <button className={styles.button_login}>LOGIN</button>
+                {/* Arte do botão login - Marinho */}
+                <Link href="/pages/cadastro"><button className={styles.button_cadastrar}>CADASTRAR</button></Link>
+                {/* Arte do botão cadastrar - Marinho */}
+                </form>
+            </div>
+            </div>
+        </div>
+    </main >
+    </>
     );
 };
 
