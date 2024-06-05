a417f105966c308b7d0f94a5b5aa73288d96a8b2
renatofreitasRR <natogfreitas@gmail.com>
2024-06-04 22:33:29 -0300

    pagina vazia

---
+++ src/app/pages/flinstonesCompra/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+import Link from 'next/link';
+import styles from '../../styles.module.css';
+
+const Perfil: React.FC = () => {
+
+
+    return (
+        <>
+            <h1>Perfil</h1>
+        </>
+    );
+};
+
+export default Perfil;
+
