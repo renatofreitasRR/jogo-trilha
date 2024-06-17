ff5f5f7e276e2ad8e91e374f36fd5e4d470c297b
renatofreitasRR <natogfreitas@gmail.com>
2024-06-16 22:43:00 -0300

    varias salas

--- src/app/pages/online/bypass/page.tsx
+++ src/app/pages/online/bypass/page.tsx
@@ -1,18 +1,47 @@
 "use client"
 
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 import { useRouter } from 'next/navigation'
 import { setLocalItem } from "@/app/utils/sessionStorage";
 import { Box, Button, FormControl, FormLabel, Heading, Input } from "@chakra-ui/react";
+import { HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
+import styles from "./styles.module.css";
 
+interface RoomsProps {
+    room: string;
+    users: number;
+}
 
 export default function ByPass() {
     const [userName, setUserName] = useState('');
     const [room, setRoom] = useState('');
+    const [rooms, setRooms] = useState<RoomsProps[]>([]);
     const router = useRouter()
 
     async function submitForm(e: FormEvent) {
         e.preventDefault();
+        router.push(`/pages/online/board/${room}`);
+    }
+
+    async function getRooms() {
+        const connection = new HubConnectionBuilder()
+            .withUrl(`${process.env.WEBSOCKET_ROUTE}/game`)
+            .configureLogging(LogLevel.Information)
+            .build();
+
+        connection.on("RoomsAvailable", (rooms) => {
+            setRooms(rooms);
+        });
+
+        await connection.start();
+        await connection.invoke("SendGroups");
+    }
+
+    useEffect(() => {
+        getRooms();
+    }, []);
+
+    function joinRoom(room: string) {
         router.push(`/pages/online/board/${room}`);
     }
 
@@ -36,6 +65,22 @@ export default function ByPass() {
                         Entrar
                     </Button>
                 </form>
+
+                <h2 className={styles.available_room_title}>Salas disponíveis</h2>
+                <div className={styles.available_room_container}>
+                    {rooms.map(room => (
+                        <button
+                            key={room.room}
+                            type="button"
+                            className={styles.available_room}
+                            onClick={() => joinRoom(room.room)}
+                        >
+                            Sala {room.room} - {room.users} usuário(s)
+                        </button>
+
+                    ))}
+                </div>
+
             </Box>
         </Box>
     );
